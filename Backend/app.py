d551da5e02c65c9b5b77ae0a4ca87e26dd3c7d53
BinduSree-18 <palukurthibindu18@gmail.com>
2026-03-03 21:04:09 +0530

    First ML Salary Predictor Project

---
+++ Backend/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)   # Allow frontend to connect
+
+# Load model
+model = joblib.load("salary_model.pkl")
+
+@app.route("/")
+def home():
+    return "Salary Predictor API is running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+
+    input_df = pd.DataFrame([{
+        "Age": float(data["age"]),
+        "Gender": data["gender"],
+        "Education Level": data["education"],
+        "Job Title": data["job_title"],
+        "Years of Experience": float(data["experience"])
+    }])
+
+    prediction = model.predict(input_df)
+
+    return jsonify({
+        "predicted_salary": int(prediction[0])
+    })
+if __name__ == "__main__":
+    app.run(debug=True)
